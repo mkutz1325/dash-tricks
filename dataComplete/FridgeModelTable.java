b445740d4b02af007a106675d2b392bb5711ccb2
Jian Zhao <jianzhao@cs.washington.edu>
2014-05-08 16:02:14 -0700

    complete data .csv and code for version 1

---
+++ dataComplete/FridgeModelTable.java
@@ -0,0 +1,15 @@
+package com.example.dash_tricks;
+/**
+ * this class define fridge model table
+ * model/capacity_freezer/ capacity-fridge/sign date
+ * @author jianzhao
+ *
+ */
+public class FridgeModelTable {
+	public static final String TABLENAME = "fridge_model";
+	public static final String ID = "id";
+	public static final String MODEL = "model";
+	public static final String FREEZER_CAPA = "freezer_capacity";
+	public static final String FRIDGE_CAPA = "fridge_capacity";
+	
+}
