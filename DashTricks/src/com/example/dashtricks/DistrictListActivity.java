82a4db1513cce66bd6122b8492c9e1bf89a4781c
Michael Kutz <mkutz1325@outlook.com>
2014-05-18 14:18:48 -0700

    stored parsing routine in Experiments, cleaned DistrictListActivity

--- DashTricks/src/com/example/dashtricks/DistrictListActivity.java
+++ DashTricks/src/com/example/dashtricks/DistrictListActivity.java
@@ -1,15 +1,11 @@
 package com.example.dashtricks;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-import com.example.dashtricks.data.DistrictList;
-import com.example.dashtricks.data.Query;
-
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.app.LoaderManager;
 
 /**
  * An activity representing a list of Districts. 
@@ -17,40 +13,17 @@ import android.support.v4.app.FragmentActivity;
  * This activity also implements the required
  * {@link DistrictListFragment.Callbacks} interface to listen for item selections.
  */
-public class DistrictListActivity extends FragmentActivity implements DistrictListFragment.Callbacks {
-	/**
-	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-	 * device.
-	 */
-//	private boolean mTwoPane;
+public class DistrictListActivity extends FragmentActivity 
+									implements DistrictListFragment.Callbacks,
+									LoaderManager.LoaderCallbacks<Cursor>{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
-		Query q = new Query(this);
-		q.open();
-		String districts = q.getAllDistricts();
-		DistrictList.setJText(districts);
-		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_district_list);
+		getLoaderManager().initLoader(0, null, this);
 
-
-
-/*		if (findViewById(R.id.metric_detail_container) != null) {
-			// The detail container view will be present only in the
-			// large-screen layouts (res/values-large and
-			// res/values-sw600dp). If this view is present, then the
-			// activity should be in two-pane mode.
-			mTwoPane = true;
-
-			// In two-pane mode, list items should be given the
-			// 'activated' state when touched.
-			((MetricListFragment) getSupportFragmentManager().findFragmentById(
-					R.id.metric_list)).setActivateOnItemClick(true);
-		}*/
-
-		// TODO: If exposing deep links into your app, handle intents here.
 	}
 
 	/**
@@ -59,23 +32,25 @@ public class DistrictListActivity extends FragmentActivity implements DistrictLi
 	 */
 	@Override
 	public void onItemSelected(String id) {
-//		if (mTwoPane) {
-//			// In two-pane mode, show the detail view in this activity by
-//			// adding or replacing the detail fragment using a
-//			// fragment transaction.
-//			Bundle arguments = new Bundle();
-//			arguments.putString(MetricDetailFragment.ARG_ITEM_ID, id);
-//			MetricDetailFragment fragment = new MetricDetailFragment();
-//			fragment.setArguments(arguments);
-//			getSupportFragmentManager().beginTransaction()
-//					.replace(R.id.metric_detail_container, fragment).commit();
-//
-//		} else {
-			// In single-pane mode, simply start the detail activity
-			// for the selected item ID.
-			Intent detailIntent = new Intent(this, MainActivity.class);
-			//detailIntent.putExtra(MetDetailFragment.ARG_ITEM_ID, id);
-			startActivity(detailIntent);
-		//}
+		Intent detailIntent = new Intent(this, MainActivity.class);
+		startActivity(detailIntent);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- DashTricks/src/com/example/dashtricks/data/DistrictList.java
+++ DashTricks/src/com/example/dashtricks/data/DistrictList.java
@@ -5,10 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+/*import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.simple.parser.ParseException;*/
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -31,21 +35,26 @@ public class DistrictList {
 
 	static {
 		// Add 3 sample items.
-		JSONParser parser=new JSONParser();
+		//JSONParser parser=new JSONParser();
 		try {
-			//Query q = new Query(null);
-			jText = "[{\"name\":\"District1\", \"id\":\"District1\"}, "
-					+ "{\"name\":\"District2\", \"id\":\"District2\"}";	
-			//JSONObject json = new JSONObject(jText);
+
+/*			//JSONObject json = new JSONObject(jText);
 			Object obj=parser.parse(jText);
 			JSONArray array=(JSONArray)obj;
 			JSONObject d1 = (JSONObject) array.get(0);
-			JSONObject d2 = (JSONObject) array.get(1);
+			JSONObject d2 = (JSONObject) array.get(1);*/
+			System.out.println(jText);
+
+			JSONTokener tokener = new JSONTokener(jText);
+
+			JSONArray array= new JSONArray(tokener);
+			JSONObject d1 = array.getJSONObject(0);
+			//JSONObject d2 = (JSONObject) d1.get("argv");
 			
 			
 			//addItem(new District((String) d1.get("id"), (String) d1.get("name")));
 			//addItem(new District((String) d2.get("id"), (String) d2.get("name")));
-		} catch (ParseException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -55,7 +64,7 @@ public class DistrictList {
 	}
 	
 	public static void setJText(String input) {
-		
+		jText = input;
 	}
 
 	private static void addItem(District d) {
