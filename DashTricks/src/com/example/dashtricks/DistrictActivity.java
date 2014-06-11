a27a6992865863caba58ef6ae6d2c922482326b4
Michael Kutz <mkutz1325@outlook.com>
2014-06-11 15:19:40 -0700

    refactoring

---
+++ DashTricks/src/com/example/dashtricks/DistrictActivity.java
@@ -0,0 +1,183 @@
+package com.example.dashtricks;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DistrictActivity extends Activity {
+
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
+	 * derivative, which will keep every loaded fragment in memory. If this
+	 * becomes too memory intensive, it may be best to switch to a
+	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+	 */
+    private String[] mSectionTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+	private CharSequence mTitle;
+	private CharSequence mDrawerTitle;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private static String districtId;
+	private static String districtName;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_district);
+
+		Bundle extras = getIntent().getExtras();
+
+		if (extras != null) {
+		    districtId = extras.getString("Id");
+		    districtName = extras.getString("Name");
+		}
+		
+		Intent mServiceIntent = new Intent(this, QueryService.class);
+		mServiceIntent.putExtra("Id", districtId);
+		mServiceIntent.putExtra("Name", districtName);
+		this.startService(mServiceIntent);
+		
+        mSectionTitles = getResources().getStringArray(R.array.drawer_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mSectionTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            @Override
+			public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            @Override
+			public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        // start with district fragment as default
+        selectItem(0);
+        // Open the drawer initially to show it is there
+        mDrawerLayout.openDrawer(mDrawerList);
+	}
+	
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_notifications) {
+			return true;
+		}
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+/*     Called whenever we call invalidateOptionsMenu() 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }*/
+	
+	// return the district id 
+	public String getDistrictId() {
+		return districtId;
+	}
+	
+	// return the district name
+	public String getDistrictName() {
+		return districtName;
+	}
+
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	        selectItem(position);
+	    }
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+	    // Create a new fragment and specify the planet to show based on position
+		Fragment fragment = null;
+		switch(position) {
+			case 0:
+				fragment = new DistrictFragment();
+				break;
+			case 1:
+			case 2:
+			case 3:
+				fragment = new CoverageFragment();
+				break;
+/*			case 2:
+				fragment = new ColdChainFragment();
+				break;
+			case 3:
+				fragment = new StockFragment();
+				break;*/
+		}
+
+	    // Insert the fragment by replacing any existing fragment
+	    FragmentManager fragmentManager = getFragmentManager();
+	    fragmentManager.beginTransaction()
+	                   .replace(R.id.content_frame, fragment)
+	                   .commit();
+
+	    // Highlight the selected item, update the title, and close the drawer
+	    mDrawerList.setItemChecked(position, true);
+	    setTitle(mSectionTitles[position]);
+	    mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+	    mTitle = title;
+	    getActionBar().setTitle(mTitle);
+	}
+}
